6be6812b3b92ad7b074e18c940f2f5b71a8c47c7
Sandiego <shirleycalipso22@gmail.com>
2025-06-17 18:01:00 -0300

    att

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='imageprocessing',
+    name='imageprocessing-sv',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
         'Pillow'
     ],
-    author='Seu Nome',
-    author_email='seuemail@example.com',
+    author='Sandiego Vieira',
+    author_email='sandiegovieira@outlook.com',
     description='Um pacote simples de processamento de imagens com Pillow',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/seunome/imageprocessing',
+    url='https://github.com/SANDIEGOVIEIRA/imageprocessing',
     classifiers=[
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
